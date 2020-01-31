cadf4445bf70f673f1b372e01d6d3b5cd9fc8f3a
Tatum Daily <tgd9019@outlook.com>
2020-01-31 15:57:22 -0600

    Pushing to Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,35 @@
 // Globals //
 // ============================================= //
 
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyA2HlMAPxQG_zjC6ZU8hwfzr7NwdQf5Y7k",
+        authDomain: "firetrain-7873c.firebaseapp.com",
+        databaseURL: "https://firetrain-7873c.firebaseio.com",
+        projectId: "firetrain-7873c",
+        storageBucket: "firetrain-7873c.appspot.com",
+        messagingSenderId: "531047566817",
+        appId: "1:531047566817:web:278ec7e864005057a4d6a0"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+
     // Element Grabbers //
     // ============================================= //
+
+        // Current Train Schedule //
+        // ============================================= //
+        var tBody = $("#tBody");
+
+        // Add Train Form //
+        // ============================================= //
+        var trainName = $("#trainName");
+        var destination = $("#destination");
+        var firstTrain = $("#firstTrain");
+        var frequency = $("#frequency");
 
     // Arrays //
     // ============================================= //
@@ -12,9 +39,30 @@
 
     // Variables //
     // ============================================= //
+    var format = "HH:mm";
+    var tableRow = $("<tr>");
+    var tableData = $("<td>");
 
 // Functions //
 // ============================================= //
+
+    // Submit Button onClick //
+    // ============================================= //
+    $("#submitBtn").on("click", function(event) {
+        event.preventDefault()
+
+        var newTrainName = trainName.val().trim();
+        var newDestination = destination.val().trim();
+        var newFirstTrain = firstTrain.val().trim();
+        var newFrequency = frequency.val().trim();
+
+        database.ref().push({
+            name: newTrainName,
+            destination: newDestination,
+            firstTrain: newFirstTrain,
+            frequency: newFrequency,
+        });
+    });
 
 // Calls //
 // ============================================= //
